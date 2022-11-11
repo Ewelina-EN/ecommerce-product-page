65af07b5dc0d7b23026c8c71195dd5a700da6d19
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2022-11-11 12:37:39 +0100

    eslint installeg

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true,
+        'node': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    'overrides': [
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'react',
+        '@typescript-eslint'
+    ],
+    'rules': {
+        'semi': ['error', 'always'],
+        'quotes': ['error', 'single', {
+            'allowTemplateLiterals': true
+        }],
+        'no-unused-vars': ['warn', {
+            'vars': 'all',
+        }]
+    }
+};
